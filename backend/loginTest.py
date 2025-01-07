411c8792ea5f6389a87a9df2c70ed116021c359a
Marcelo Dávila <dmarcelo201@gmail.com>
2025-01-07 16:48:54 -0400

    admin

--- backend/loginTest.py
+++ backend/loginTest.py
@@ -4,7 +4,7 @@ import requests
 URL_BASE = "https://apielektadev.fos.com.bo/api"
 
 # Credenciales de prueba
-CREDENCIALES_VALIDAS = {"email": "admin@fos.com.bo", "password": "12345678"}
+CREDENCIALES_VALIDAS = {"email": "admin@fos.com.bo", "password": "admin"}
 DOMINIO_INCORRECTO = {"email": "admin@fost.com.bo", "password": "12345678"}
 CREDENCIALES_INVALIDAS = {"email": "user@fos.com.bo", "password": "admins"}
 
