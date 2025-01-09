7e15247cb6dd275939209ac0318541077e6d3c79
Marcelo Dávila <dmarcelo201@gmail.com>
2025-01-09 09:17:59 -0400

    retest

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
     try:
         # login
         print("Bienvenido, empezaremos estas pruebas con el endpoint de login")
-        token = test_login_exitoso()
+        test_login_exitoso()
         test_login_fallido()
         test_login_correo_invalido()
         test_probar_campos_faltantes()
         print("Ahora probaremos con el endpoint de roles")
-        test_listado_roles(token)
+        test_listado_roles()
         print("¡Todas las pruebas pasaron exitosamente!")
     except AssertionError as e:
         print(f"Prueba fallida: {e}")
