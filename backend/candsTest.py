3f22e4d0d6f44854d905327e2a0d14653e321d6a
Marcelo Dávila <dmarcelo201@gmail.com>
2025-01-15 09:09:56 -0400

    asd

---
+++ backend/candsTest.py
@@ -0,0 +1,24 @@
+import json
+import random
+import pytest
+import requests
+from faker import Faker
+
+fake = Faker('es_MX')
+
+# Definir constantes
+URL_BASE = "https://apielektadev.fos.com.bo/api"
+URL_CANDIDATOS = URL_BASE + "/canididates"
+
+#Obtener el token para las funciones
+def obtenerToken():
+    response = requests.post(f"{URL_BASE}/adm-login", json={"email": "admin@fos.com.bo", "password": "12345678"})
+    response.raise_for_status()
+    datos = response.json()
+    return datos['data']['token']
+
+def obtenerTokenSoporte():
+    response = requests.post(f"{URL_BASE}/adm-login", json={"email": "support@fos.com.bo", "password": "12345678"})
+    response.raise_for_status()
+    datos = response.json()
+    return datos['data']['token']
---
+++ backend/typeCandsTest.py
@@ -0,0 +1,19 @@
+import json
+import random
+import pytest
+import requests
+from faker import Faker
+
+fake = Faker('es_MX')
+
+# Definir constantes
+URL_BASE = "https://apielektadev.fos.com.bo/api"
+URL_TIPO_CAND = URL_BASE + "/typecands"
+
+#Obtener el token para las funciones
+def obtenerToken():
+    response = requests.post(f"{URL_BASE}/adm-login", json={"email": "admin@fos.com.bo", "password": "12345678"})
+    response.raise_for_status()
+    datos = response.json()
+    return datos['data']['token']
+
