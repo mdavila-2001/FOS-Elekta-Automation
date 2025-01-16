1f312bb8da1dfc5ca00a30e83b9f742f6029d1a4
Marcelo <dmarcelo201@gmail.com>
2025-01-16 13:28:42 -0400

    educationLevel

--- backend/eduLevel.py
+++ backend/eduLevel.py
@@ -8,7 +8,23 @@ fake = Faker('es_MX')
 
 # Definir constantes
 URL_BASE = "https://apielektadev.fos.com.bo/api"
-URL_EDU_LEVEL = URL_BASE + "/canididates"
+URL_EDU_LEVEL = URL_BASE + "/educations"
+
+# Datos necesarios
+educacion = {
+    "client_id": 1,
+    "name": "Universidad",
+}
+
+educacion_edit = {
+    "client_id": 1,
+    "name": "Universitario",
+}
+
+params = {
+    "client_id": 1,
+    "fullType": "L"
+}
 
 #Obtener el token para las funciones
 def obtenerToken():
@@ -17,8 +33,52 @@ def obtenerToken():
     datos = response.json()
     return datos['data']['token']
 
-def obtenerTokenSoporte():
-    response = requests.post(f"{URL_BASE}/adm-login", json={"email": "support@fos.com.bo", "password": "12345678"})
-    response.raise_for_status()
-    datos = response.json()
-    return datos['data']['token']
+def test_get_educations():
+    try:
+        token = obtenerToken()
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.get(URL_EDU_LEVEL, headers=headers, params=params)
+        response.raise_for_status()
+        assert response.status_code == 200
+        assert response.json()['data']
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+
+@pytest.fixture(scope="module")
+def post_education():
+    try:
+        token = obtenerToken()
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.post(URL_EDU_LEVEL, json=educacion, headers=headers)
+        response.raise_for_status()
+        return response.json()['data']
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+
+def test_update_education(post_education):
+    try:
+        token = obtenerToken()
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.put(f"{URL_EDU_LEVEL}/{post_education['id']}", json=educacion_edit, headers=headers)
+        response.raise_for_status()
+        assert response.status_code == 200
+        assert response.json()['message'] == "Registro actualizado con éxito"
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+
+# def test_delete_education(post_education):
+#     try:
+#         token = obtenerToken()
+#         headers = {"Authorization": f"Bearer {token}"}
+#         response = requests.delete(f"{URL_EDU_LEVEL}/{post_education['id']}", headers=headers, params={"client_id": 1})
+#         response.raise_for_status()
+#         assert response.status_code == 200
+#         assert response.json()['message'] == "Registro eliminado con éxito"
+#     except requests.exceptions.HTTPError as http_err:
+#         print(f"HTTP error occurred: {http_err}")
