3b9c1bdb216c2e12f68a2d1c04a67db2b2d3febd
Marcelo <dmarcelo201@gmail.com>
2025-01-21 13:26:25 -0400

    asd

---
+++ backend/areaTest.py
@@ -0,0 +1,90 @@
+import json
+import pytest
+import requests
+
+# Definir constantes
+URL_BASE = "https://apielektadev.fos.com.bo/api"
+
+#Datos necesarios
+LISTADO = {"fullType": "L"}
+
+AREA = {
+    "name": "RR.HH."
+}
+
+AREA_EDITADA = {
+    "name": "Recursos Humanos"
+}
+
+def obtenerToken():
+    response = requests.post(f"{URL_BASE}/adm-login", json={"email": "admin@fos.com.bo", "password": "12345678"})
+    response.raise_for_status()
+    datos = response.json()
+    return datos['data']['token']
+
+# Pruebas para la lectura de áreas
+def test_obtener_areas():
+    try:
+        token = obtenerToken()
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.get(f"{URL_BASE}/areas", headers=headers, params=LISTADO)
+        response.raise_for_status()
+        datos = response.json()
+        assert response.status_code == 200
+        print(f"Áreas obtenidas: {json.dumps(datos['data'], indent=4)}")
+    except requests.exceptions.HTTPError as err:
+        print(f"Prueba fallida - {err}")
+
+# Prueba para crear un área
+@pytest.fixture(scope="module")
+def crear_area():
+    try:
+        token = obtenerToken()
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.post(f"{URL_BASE}/areas", json=AREA, headers=headers)
+        response.raise_for_status()
+        datos = response.json()
+        assert response.status_code == 200
+        print(f"Área creada con el ID: {datos['data']}")
+        return datos['data']
+    except requests.exceptions.HTTPError as err:
+        print(f"Prueba fallida - {err}")
+
+def test_buscar_area(crear_area):
+    try:
+        token = obtenerToken()
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.get(f"{URL_BASE}/areas/{crear_area}", headers=headers)
+        response.raise_for_status()
+        datos = response.json()
+        assert response.status_code == 200
+        print(f"Área buscada: {json.dumps(datos['data'], indent=4)}")
+    except requests.exceptions.HTTPError as err:
+        print(f"Prueba fallida - {err}")
+
+# Prueba para editar un área
+def test_editar_area(crear_area):
+    try:
+        token = obtenerToken()
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.put(f"{URL_BASE}/areas/{crear_area}", json=AREA_EDITADA, headers=headers)
+        response.raise_for_status()
+        datos = response.json()
+        assert response.status_code == 200
+        assert datos['message'] == "Registro actualizado con éxito", "No se logró actualizar la área"
+        print(f"Área editada: {json.dumps(datos, indent=4)}")
+    except requests.exceptions.HTTPError as err:
+        print(f"Prueba fallida - {err}")
+
+# Prueba para eliminar un área
+def test_eliminar_area(crear_area):
+    try:
+        token = obtenerToken()
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.delete(f"{URL_BASE}/areas/{crear_area}", headers=headers)
+        response.raise_for_status()
+        datos = response.json()
+        assert response.status_code == 200
+        assert datos['message'] == "Registro eliminado con éxito", "No se logró eliminar la área"
+    except requests.exceptions.HTTPError as err:
+        print(f"Prueba fallida - {err}")
