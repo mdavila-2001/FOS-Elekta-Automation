c66e1e09b1b1327b32902c72afe56120671c6193
Marcelo Dávila <dmarcelo201@gmail.com>
2025-01-29 14:28:00 -0400

    asd

--- backend/usersTest.py
+++ backend/usersTest.py
@@ -119,7 +119,7 @@ def test_editar_usuario(crear_usuario):
         response.raise_for_status()
         datos = response.json()
         assert datos['message'] == "Registro actualizado con éxito", "No se logró actualizar el usuario"
-        print(json.dumps(datos['Debug_Querys'][-1], indent=4))
+        print(json.dumps(datos, indent=4))
     except requests.exceptions.HTTPError as e:
         pytest.fail(f"editar_usuario: Prueba fallida - {e}")
 
